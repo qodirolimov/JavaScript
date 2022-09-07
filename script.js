27970431f4a7c2fea7c3fbc6caeffc06d649e206
qodirolimov <qodirolimov88@gmail.com>
2022-09-07 22:52:58 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict"
+
+
+
+const header=$('header'),
+nav=$('nav'),
+logo=$('.nav-logo'),
+section=$('.section'),
+wrapper=$('.wrapper');
+const item=$a('.nav__ul--item');
+
+
+
+
+
+
+ const div=document.createElement('div');
+ div.setAttribute('class', 'div1');
+ div.innerHTML='<p>Lorem ipsum</p>';
+
+
+
+
+
+const div4=createElement('div', 'ovvk',  "<h4>244</h4>");
+const box=createElement('div', 'box',  "<h4>22</h4>");
+
+wrapper.appendChild(div4)
